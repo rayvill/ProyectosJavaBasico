16292190546d0124d1ef2539aac86d085741d091
rayvill <93739568+rayvill@users.noreply.github.com>
2025-04-23 00:28:22 -0400

    Add files via upload

---
+++ condicionales.java
@@ -0,0 +1,24 @@
+public class condicionales{
+   public static void main(String[] args){
+    int edad=18;
+    if(edad>=18){
+        System.out.println("eres mayor de edad");
+
+    }
+    else{ 
+        System.out.println("Eres menor de edad");
+
+    }
+
+    int nota = 85;
+
+    if (nota >= 90) {
+        System.out.println("Excelente");
+    } else if (nota >= 70) {
+        System.out.println("Aprobado");
+    } else {
+        System.out.println("Reprobado");
+    }    
+   }
+
+}
